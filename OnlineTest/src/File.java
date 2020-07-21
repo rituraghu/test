592a2a7ec04fbd428a60e4e29a0a7d3f8942e365
viplav <viplav@DESKTOP-2L7ORB9>
2020-07-21 21:36:23 +0530

    ok

--- OnlineTest/src/File.java
+++ OnlineTest/src/File.java
@@ -1,4 +1,52 @@
+import org.apache.poi.hssf.model.Workbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
 
 public class File {
 
+	class main{
+	 private File outputFile;
+	 private String  destinationFolder;
+
+	 public  main(){
+	    this.destinationFolder="test";
+	 }
+	 public void fileIteration(){
+	    try{
+	        for(File fileEntry :  new File(this.destinationFolder).listFiles()){
+
+	            if (fileEntry.isDirectory()){
+
+	            }
+	            else{
+	                System.out.println(fileEntry.getName());
+	                this.excelManipulate(fileEntry.getName());
+	            }
+	        }
+	    }
+
+	    catch(Exception e){
+	        e.printStackTrace();
+	    }
+	 }
+
+
+	 public void excelManipulate(String inputFile){
+	 try{
+	     System.out.println(inputFile);
+	     Workbook workbook=Workbook.createWorkbook(new File("test/"+inputFile));
+	     Sheet sheet=workbook.getSheetIndex(0);
+	     Cell a5=((Object) sheet).getCell("a1");
+	     System.out.println(((Object) a5).getContents());;
+	 }
+	 catch(Exception e){
+	    System.out.println(e.getMessage());
+	 }
+	 }
+
+	 public void  main(String[] args){
+	    main ofh=new main();
+	    ofh.fileIteration();
+	    }
+	 }
 }
